2200d8d0339f139c24f41b7ff1fb4f4d90f8adcf
royal9619@gmail.com <naknin-byvpyg-0pIcra>
2026-04-01 16:59:09 -0600

    SnackBars

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -27,4 +27,16 @@ const appMenuItems = <MenuItem>[
     link: '/cards',
     icon: Icons.credit_card,
   ),
+  MenuItem(
+    title: 'ProgressIndicators',
+    subtitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
+  ),
+  MenuItem(
+    title: 'Snackbars y dialogos',
+    subtitle: 'Indicadores en pantalla',
+    link: '/snackbars',
+    icon: Icons.info_outline,
+  ),
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -4,8 +4,27 @@ class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    final snackbar = SnackBar(
+      content: const Text('Hola Mundo batman'),
+      action: SnackBarAction(label: 'Ok', onPressed: () {}),
+      duration: Duration(seconds: 2),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Snackbars y Diálogos')),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () {
+          showCustomSnackbar(context);
+        },
+      ),
+    );
   }
 }
